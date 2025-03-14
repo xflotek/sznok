91cc43ba2ac7bc366b25075607b05e207083d25a
xflotek <kacperbuczak294@gmail.com>
2025-03-14 22:01:43 +0100

    Add particles on bullet hit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,12 +10,17 @@ public class Player : MonoBehaviour
     private float m_MaxHealth = 100f;
     [SerializeField] 
     private float m_MovementSpeed = 1f;
+    [SerializeField]
+    private Vector3 m_DefaultScale = new(1.5f, 1.5f, 1f);
+    private Vector3 m_MoveLeft = new(-1, 1, 1);
+    private Vector3 m_MoveRight = new(1, 1, 1);
     private InputAction m_MoveAction;
     private Rigidbody2D m_Rb;
     private Vector2 m_VecMvmt;
     private Weapon m_Weapon;
     private bool m_ShouldFire;
-    public float m_LastDirection;
+    private Animator m_Animator;
+    private float m_Moving;    
 
     public static UnityEvent onPlayerDeath;
 
@@ -37,14 +42,42 @@ public class Player : MonoBehaviour
         m_Weapon = GetComponent<Weapon>();
         m_ShouldFire = false;
         onPlayerDeath ??= new();
+        m_Animator = GetComponent<Animator>();
+        transform.localScale = m_DefaultScale;
     }
 
     void Update()
     {
+        Debug.Log(m_VecMvmt.x);
         m_VecMvmt = m_MoveAction.ReadValue<Vector2>();
+
+        if (m_VecMvmt.x != 0 || m_VecMvmt.y != 0) {
+            m_Animator.SetBool("isMoving", true);
+
+        } else {
+            m_Animator.SetBool("isMoving", false);
+        }
+
         if (Keyboard.current.fKey.ReadValue() > 0) {
             m_ShouldFire = true;
         }
+
+        if (m_VecMvmt.x > 0) {
+            transform.localScale = new Vector3(
+                m_DefaultScale.x * m_MoveRight.x,
+                m_DefaultScale.y * m_MoveRight.y,
+                m_DefaultScale.z * m_MoveRight.z
+            );
+        }
+
+        if (m_VecMvmt.x < 0) {
+            transform.localScale = new Vector3(
+                m_DefaultScale.x * m_MoveLeft.x,
+                m_DefaultScale.y * m_MoveLeft.y,
+                m_DefaultScale.z * m_MoveLeft.z
+            );
+        }
+
     }
 
     void FixedUpdate()

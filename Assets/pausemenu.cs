5380b967682de50db41d7aa0b2fa1b2c585e8ffa
tysia <163890963+ruraplusplus@users.noreply.github.com>
2025-03-15 02:28:34 +0100

    chuj

---
+++ Assets/pausemenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pausemenu : MonoBehaviour
+{
+    public GameObject pause;
+    public bool ispaused;
+    void Start()
+    {
+        pause.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (ispaused)
+            {
+                resumegame();
+            }
+            else
+            {
+                pausegame();
+            }
+        }
+    }
+
+    public void pausegame()
+    {
+        pause.SetActive(true);
+        Time.timeScale = 0f;
+        ispaused = true;
+    }
+    public void resumegame()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1f;
+        ispaused = false;
+    }
+
+    public void restartgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
